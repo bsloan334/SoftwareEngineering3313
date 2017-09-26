588756ac46a7c11e175df8815607629b97fb5091
miguel toledo <toledomiguel5@gmail.com>
2017-09-26 15:34:03 -0400

    adding database configs and views

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+const user = {name: 'bert'}
 
 app.get('/', (req, res) => {
-    console.log('--------------');
-    console.log(req);
-    res.render('./index.html');
+    res.render('index', {user: ''});
 });
 
 app.get('*', (req, res) => {
